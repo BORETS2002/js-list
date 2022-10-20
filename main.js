788b8e57aa714a06d6ca5b27074af412dd3f8ee1
BORETS2002 <BORETS2002@example.com>
2022-10-21 00:06:15 +0300

    finish

---
+++ main.js
@@ -0,0 +1,55 @@
+let Elinput = document.querySelector(".form-control");
+let Ellist = document.querySelector(".list")
+let ElBtn = document.querySelector(".btn")
+
+let arr  =[]
+let son = 1 
+
+ElBtn.addEventListener("click", function ( ){
+  
+  let obj = {
+    text:Elinput.value
+  }
+  arr.push(obj)
+Elinput.value = "" 
+
+  let id = document.createElement("span");
+ id.setAttribute("class", "me-2");
+
+  id.textContent = son++;
+
+  
+ const li =  document.createElement("li")
+ li.setAttribute("class" , "items" )
+
+
+ const elCheck =  document.createElement("input")
+ elCheck.classList.add("form-check-input", "ms-3" , "me-2");
+ elCheck.setAttribute("type", "checkbox");
+ elCheck.setAttribute("class", "check");
+
+
+ const elDelet =  document.createElement("button")
+ elDelet.setAttribute("class" , "btndel" )
+ elDelet.textContent="delete"
+
+ const elAdd =  document.createElement("button")
+ elAdd.setAttribute("class" , "btnAd" )
+ elAdd.textContent="Add"
+
+
+ 
+ li.textContent = obj.text
+ li.prepend(id)
+ li.append(elCheck)
+ li.appendChild(elDelet)
+ li.appendChild(elAdd)
+
+ Ellist.appendChild(li)
+ 
+ 
+
+
+} )
+
+
---
+++ test.js
@@ -0,0 +1,62 @@
+
+// const chaqirish =  x  =>  document.querySelector(`${x}`)  
+
+
+//   function uzun (x) {
+//   let arr = [x]
+//  console.log(arr);
+//   }
+//  let a = "man hozir kodlayabman"
+
+//   console.log(  uzun(a) );
+
+
+const trip = [
+
+  {
+    names : "Jek",
+    cost : 1000
+  },
+  
+  {
+    names :" Varabey",
+    cost : 4000
+  },
+  
+  {
+    names : "Huk",
+    cost : 500
+  },
+  
+  {
+    names : "Smiit",
+    cost : 50
+  },
+  ]
+  
+  // function summa (x){
+  // let a = 0
+  //   for (let i = 0; i < x.length; i++) {
+  //    a += x[i].cost
+    
+  //   }
+  //   return console.log(a);
+  // }
+  // summa(trip)
+  
+  
+  
+  // function summa (x){
+  //   for (let i = 0; i < x.length; i++) {
+  //    let  a = x[i]
+  //    Math.round(a[i])
+  // let b = [a]
+  // return console.log(b);
+  
+  //   }
+  // }
+  
+  // a = [ 0 , 1.2, 2.2, 3.4, 5.6]
+  
+  // summa(a)
+  
